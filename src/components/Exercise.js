836af88488943a3b6739f5cc1f50c7ccd579c89e
giseleml <giselemaraleo@gmail.com>
2019-02-01 18:05:24 -0200

    Fixing Reducer Connection

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -4,7 +4,6 @@ import { toggleSolution } from '../actions/exercise'
 
 
 const Exercise = (props) => {
-  console.log("props", props)
   return (
   <div className="exercise">
     <h3 className="exercise__title">{props.title}</h3>
@@ -16,4 +15,6 @@ const Exercise = (props) => {
 }
 
 
-export default connect((state, ownProps) => { console.log("state", state); console.log("ownProps", ownProps); return state }, {toggleSolution})(Exercise)
+export default connect((state, ownProps) => (
+  {...ownProps, ...state.exercise} ) ,
+  {toggleSolution})(Exercise)
