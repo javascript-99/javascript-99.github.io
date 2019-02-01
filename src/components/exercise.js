e588c0d2408075f6921313ab0cd373ed6d5bea59
Wagner Leonardi <leonardiwagner@gmail.com>
2019-02-01 17:16:52 -0200

    Update exercise.js

--- src/components/exercise.js
+++ src/components/exercise.js
@@ -3,14 +3,18 @@ import { connect } from 'react-redux'
 import { toggleSolution } from '../actions/exercise'
 
 
-const Exercise = (props) => (
-<div className="exercise">
-  <h3 className="exercise__title">{props.title}</h3>
-  <p className="exercise__description">{props.description}</p>
-  <button onClick={() => props.toggleSolution()}>Show Answer</button>
-  {props.showAnswer ? <p className="exercise__solution">{props.solution}</p> : undefined}
-</div>
-)
+const Exercise = (props) => {
+  console.log("props", props)
+  return (
+  <div className="exercise">
+    <h3 className="exercise__title">{props.title}</h3>
+    <p className="exercise__description">{props.description}</p>
+    <button onClick={() => props.toggleSolution()}>Show Answer</button>
+    {props.showAnswer ? <p className="exercise__solution">{props.solution}</p> : undefined}
+  </div>
+  )
+}
+  
 
 export default connect(
   (state) => state,
